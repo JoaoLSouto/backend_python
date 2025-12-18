e5fc0460308ddd1285613bdf7d44968177b70ba1
joaolsouto <contato.joaolsouto@gmail.com>
2025-12-18 13:06:07 -0300

    add: implement routes

--- API com flask/src/app.py
+++ API com flask/src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for
 
 app = Flask(__name__)
 
@@ -6,3 +6,28 @@ app = Flask(__name__)
 @app.route("/helloworld")
 def hello_world():
     return "<h1>Hello, World!</h1>"
+
+
+@app.route("/bemvindo/<usuario>/<int:idade>")
+def bem_vindo(usuario, idade):
+    print(idade)
+    print(f"tipo da variável idade: {type(idade)}")
+    print(f"tipo da variável usuário: {type(usuario)}")
+    return f"<h1>Bem vindo! usuário: {usuario} </h1>"
+
+
+@app.route("/projects/")
+def projects():
+    return "The Project Page"
+
+
+@app.route("/about", methods=["Get", "POST"])
+def about():
+    return "The About Page"
+
+
+with app.test_request_context():
+    print(url_for("hello_world"))
+    print(url_for("projects"))
+    print(url_for("about", next="/"))
+    print(url_for("bem_vindo", usuario="joao", idade=24))
