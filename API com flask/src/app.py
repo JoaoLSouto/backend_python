71ac942aef98486ecc1f0de93cdcbd7183d7aeab
joaolsouto <contato.joaolsouto@gmail.com>
2025-12-20 13:57:19 -0300

    add: implement database setup and schema

--- API com flask/src/app.py
+++ API com flask/src/app.py
@@ -5,10 +5,7 @@ from flask import Flask
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY="dev",
-        DATABASE=os.path.join(app.instance_path, "diobank.sqlite"),
-    )
+    app.config.from_mapping(SECRET_KEY="dev", DATABASE="diobank.sqlite")
 
     if test_config is None:
         app.config.from_pyfile("config.py", silent=True)
@@ -16,10 +13,8 @@ def create_app(test_config=None):
         # load the test config if passed in
         app.config.from_mapping(test_config)
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+    from . import db
+
+    db.init_app(app)
 
     return app
