9cdf1728042eb382d2da94742277cd9a72d25e95
joaolsouto <contato.joaolsouto@gmail.com>
2026-01-02 13:02:43 -0300

    add: create initial migration with role, user, and post tables; add password field to user model

--- API com flask/src/app.py
+++ API com flask/src/app.py
@@ -28,6 +28,7 @@ class Role(db.Model):
 class User(db.Model):
     id: Mapped[int] = mapped_column(sa.Integer, primary_key=True)
     username: Mapped[str] = mapped_column(sa.String, unique=True)
+    password: Mapped[str] = mapped_column(sa.String, nullable=False)
     role_id: Mapped[int] = mapped_column(sa.ForeignKey("role.id"))
     role: Mapped["Role"] = relationship(back_populates="user")
     # active: Mapped[bool] = mapped_column(sa.Boolean, default=True)
