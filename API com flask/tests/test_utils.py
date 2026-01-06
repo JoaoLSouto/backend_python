43110c5a7d7f733a970e32d9e49d0b1adf841a17
joaolsouto <contato.joaolsouto@gmail.com>
2026-01-05 23:04:03 -0300

    add: refactor tests for eleva_quadrado function to use parameterized inputs and improve error handling

--- API com flask/tests/test_utils.py
+++ API com flask/tests/test_utils.py
@@ -1,6 +1,17 @@
 from src.utils import eleva_quadrado
+import pytest
 
 
-def test_eleva_quarado_sucesso():
-    resultado = eleva_quadrado(2)
-    assert resultado == 4
+@pytest.mark.parametrize("test_input,expected", [(2, 4), (10, 100), (3, 9)])
+def test_eleva_quarado_sucesso(test_input, expected):
+    resultado = eleva_quadrado(test_input)
+    assert resultado == expected
+
+
+@pytest.mark.parametrize("test_input,expected", [(2, 4), (10, 100), (3, 9)])
+def test_eleva_quadrado_falha():
+    with pytest.raises(TypeError) as exc:
+        eleva_quadrado("a")
+    assert (
+        str(exc.value) == "unsupported operand type(s) for ** or pow(): 'str' and 'int'"
+    )
