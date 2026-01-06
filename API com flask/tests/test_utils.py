26ff8e62af9aa33d429be84a5ca36688e13ae704
joaolsouto <contato.joaolsouto@gmail.com>
2026-01-06 11:19:10 -0300

    add: update dependencies in pyproject.toml to include pytest-mock and enhance test_utils.py with additional requires_role tests

--- API com flask/tests/test_utils.py
+++ API com flask/tests/test_utils.py
@@ -1,6 +1,6 @@
 from src.utils import eleva_quadrado, requires_role
 import pytest
-from unittest.mock import Mock, patch
+from unittest.mock import patch
 from http import HTTPStatus
 
 
@@ -8,16 +8,6 @@ from http import HTTPStatus
 def test_eleva_quarado_sucesso(test_input, expected):
     resultado = eleva_quadrado(test_input)
     assert resultado == expected
-
-
-# @pytest.mark.parametrize("test_input,expected", [(2, 4), (10, 100), (3, 9)])
-# def test_eleva_quadrado_falha():
-#     with pytest.raises(TypeError) as exc:
-#         eleva_quadrado("a")
-#     assert (
-#         str(exc.value) == "unsupported operand type(s) for ** or pow(): 'str' "
-#         "and 'int'"
-#     )
 
 
 def test_requires_role():
@@ -38,3 +28,28 @@ def test_requires_role():
 
     mock_db_get_or_404.stop()
     mock_get_jwt_identity.stop()
+
+
+def test_requires_role_sucess(mocker):
+    mock_user = mocker.Mock()
+    mock_user.role.name = "admin"
+
+    mocker.patch("src.utils.get_jwt_identity"),
+    mocker.patch("src.utils.db.get_or_404", return_value=mock_user)
+
+    decorated_function = requires_role("admin")(lambda: "success")
+    result = decorated_function()
+    assert result == "success"
+
+
+def test_requires_role_fail(mocker):
+    mock_user = mocker.Mock()
+    mock_user.role.name = "normal"
+
+    mocker.patch("src.utils.get_jwt_identity"),
+    mocker.patch("src.utils.db.get_or_404", return_value=mock_user)
+
+    decorated_function = requires_role("admin")(lambda: "success")
+    result = decorated_function()
+
+    assert result == ({"message": "User dont have access."}, HTTPStatus.FORBIDDEN)
