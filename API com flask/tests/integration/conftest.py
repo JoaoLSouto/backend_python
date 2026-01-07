5236bd36ac3ad9003bf57700f42bcaefb83fecd1
joaolsouto <contato.joaolsouto@gmail.com>
2026-01-07 01:20:13 -0300

    add: enhance conftest.py with access_token fixture and update test_user.py with user creation and listing tests

--- API com flask/tests/integration/conftest.py
+++ API com flask/tests/integration/conftest.py
@@ -1,6 +1,5 @@
 import pytest
-from src.app import create_app
-from src.db import db
+from src.app import create_app, db, User, Role
 
 
 @pytest.fixture()
@@ -17,10 +16,23 @@ def app():
     with app.app_context():
         db.create_all()
         yield app
-        db.session.remove()
         db.drop_all()
 
 
 @pytest.fixture()
 def client(app):
     return app.test_client()
+
+@pytest.fixture()
+def access_token(client):
+        
+    role = Role(name='admin')
+    db.session.add(role)
+    db.session.commit()
+
+    user = User(username='john-doe', password='test', role_id=role.id)
+    db.session.add(user)
+    db.session.commit()
+
+    response = client.post('/auth/login', json={"username": user.username, "password": user.password})
+    return response.json['access_token']
--- API com flask/tests/integration/controllers/test_user.py
+++ API com flask/tests/integration/controllers/test_user.py
@@ -1,4 +1,5 @@
 from http import HTTPStatus
+from sqlalchemy import func
 from src.app import Role, User, db
 
 def test_get_user_success(client):
@@ -25,3 +26,43 @@ def test_get_user_success(client):
     response = client.get(f"/users/{user_id}")
 
     assert response.status_code == HTTPStatus.NOT_FOUND
+
+def test_create_user(client, access_token):
+
+    role_id = db.session.execute(db.select(Role.id).where(Role.name == "admin")).scalar()
+    payload = {"username": "user2", "password": "user2", "role_id": role_id}
+
+    response = client.post("/users/", json=payload, headers={'Authorization': f"Bearer {access_token}"})
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json == {"message": "User created!"}
+    assert db.session.execute(db.select(func.count(User.id))).scalar() == 2
+
+def test_list_users(client):
+
+    role = Role(name='admin')
+    db.session.add(role)
+    db.session.commit()
+
+    user = User(username='john-doe', password='test', role_id=role.id)
+    db.session.add(user)
+    db.session.commit()
+
+    response = client.post('/auth/login', json={"username": user.username, "password": user.password})
+    access_token = response.json['access_token']
+
+    response = client.get("/users/", headers={'Authorization': f"Bearer {access_token}"})
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json == {
+        "users": [
+            {
+                "id": user.id,
+                "username": user.username,
+                "role": {
+                    "id": user.role.id,
+                    "name": user.role.name,
+                },
+            }
+        ]
+    }
